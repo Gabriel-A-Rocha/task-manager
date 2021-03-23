343b2a448d3933e4832176d2e5d68f8a0e63e7b5
Gabriel Rocha <gabriel.altair.rocha@gmail.com>
2021-03-23 17:00:36 -0400

    fix: remove duplicate port listening command

--- src/index.js
+++ src/index.js
@@ -8,14 +8,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// workaround to prevent tests clash with the same port
-function getRandomPort(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-const port = getRandomPort(3333, 5555);
-app.listen(port, () => console.log(`Server started at port ${port}`));
-
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
@@ -57,7 +49,6 @@ app.post("/users", (request, response) => {
 
 app.get("/todos", checksExistsUserAccount, (request, response) => {
   const { foundUser } = request;
-  console.log("🚀 ~ file: index.js ~ line 62 ~ app.get ~ foundUser", foundUser);
 
   return response.status(200).json(foundUser.todos);
 });
